fd4ce564437206f2d4e769cdea7f0c5cd79940bb
Blake <38326327+cblakesanders@users.noreply.github.com>
2021-02-05 21:43:07 -0500

    Update settings.py

--- library/settings.py
+++ library/settings.py
@@ -142,4 +142,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sanders.blake10@gmail.com'
-EMAIL_HOST_PASSWORD = 'Logmein3'
+EMAIL_HOST_PASSWORD = '********'
